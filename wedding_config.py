3a0a44eb82664be65689dc4d2ef9a5c8b8650ba1
KHS-Wedding <hyeonsu.kim.kr@gmail.com>
2026-07-27 18:09:36 +0900

    Add files via upload

--- wedding_config.py
+++ wedding_config.py
@@ -11,7 +11,7 @@ WEDDING = {
         'url': 'https://khs-wedding.github.io/invitation/',
         'share_url': 'https://khs-wedding.github.io/invitation/',
         'share_image': 'static/share-preview-v15.png',
-        'asset_version': '21',
+        'asset_version': '22',
         'draft_notice': '',
     },
 
